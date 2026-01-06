45fbc61d99ff7b5b49a0c665981bfa117c6c1b49
chihung1024 <104315542+chihung1024@users.noreply.github.com>
2026-01-06 23:42:35 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import './index.css' // <--- 新增這一行
 
 const app = createApp(App)
 app.use(createPinia())
