1b09865211edaeaf31dce17fb30a82d1b8069c72
chihung1024 <104315542+chihung1024@users.noreply.github.com>
2026-08-18 12:02:27 +0800

    feat: show canonical import receipt

--- src/components/BrokerNeutralImportPreview.vue
+++ src/components/BrokerNeutralImportPreview.vue
@@ -196,6 +196,7 @@
               <strong>{{ resultTitle }}</strong>
               <span>{{ resultMessage }}</span>
             </div>
+            <ImportReconciliationReceipt :result="result" />
           </div>
 
           <footer class="dialog-footer">
@@ -239,6 +240,7 @@ import { CONFIG } from '../config.js';
 import { useToast } from '../composables/useToast';
 import { useAuthStore } from '../stores/auth';
 import { usePortfolioStore } from '../stores/portfolio';
+import ImportReconciliationReceipt from './ImportReconciliationReceipt.vue';
 import {
   MAX_CANONICAL_CSV_BYTES,
   OPTIONAL_CANONICAL_HEADERS,
